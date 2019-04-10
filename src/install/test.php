5747e9364f4b743ef6a54f2d2a2d99ffc39ea342
dropdead <dropdead@hashcat.net>
2019-04-10 10:54:17 +0200

    updated unattended sql install

---
+++ src/install/test.php
@@ -0,0 +1,11 @@
+<?php
+
+use DBA\AccessGroupUser;
+use DBA\Config;
+use DBA\QueryFilter;
+use DBA\RightGroup;
+use DBA\User;
+
+require_once(dirname(__FILE__) . "/../inc/load.php");
+
+
--- src/install/unattended_install.php
+++ src/install/unattended_install.php
@@ -6,12 +6,31 @@ use DBA\QueryFilter;
 use DBA\RightGroup;
 use DBA\User;
 
-require_once(dirname(__FILE__) . "/../inc/load.php");
 
+@include(dirname(__FILE__) . "/../inc/conf.php");
 
+      #global $CONN;
+      $dsn = 'mysql:host=' . $CONN['server'] . ";port=" . $CONN['port'];
+      $user = $CONN['user'];
+      $password = $CONN['pass'];
+
+    try {
+      $dbh = new PDO($dsn, $user, $password);
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $dbh->exec('create database htp');
+
+      $dbh->exec('use htp');
 
       $query = file_get_contents(dirname(__FILE__) . "/hashtopolis.sql");
-      $FACTORIES::getAgentFactory()->getDB()->query($query);
-      #$FACTORIES::getConfigFactory()->save($urlConfig);
+      $dbh->exec($query);
 
+      $query = file_get_contents(dirname(__FILE__) . "/unattended.sql");
+      $dbh->exec($query);
+    }
+    catch (PDOException $e) {
+      if ($test) {
+        return null;
+      }
+      print("Fatal Error ! Database connection failed: " . $e->getMessage());
+    }
 
